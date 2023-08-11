644941b818acabab4eddf2d323c3a322f0d23ec6
Eric Delmer Millen <ericdelmermillen@gmail.com>
2023-08-11 12:42:46 -0400

    commit before oepning on Ali's mac

--- routes/gabs.js
+++ routes/gabs.js
@@ -8,7 +8,7 @@ const madGabsHardPath = './data/madGabsHard.json';
 const { v4: uuidv4 } = require('uuid');
 
 
-// get videos request
+// get easy gabs route
 router.route('/easy')
 
   .get((req, res) => {
@@ -24,7 +24,7 @@ router.route('/easy')
   })
 });
 
-// get videos request
+// get medium gabs route
 router.route('/medium')
   .get((req, res) => {
     console.log("medium")
@@ -39,7 +39,7 @@ router.route('/medium')
   })
 });
 
-// get videos request
+// get hard gabs route
 router.route('/hard')
   .get((req, res) => {
     console.log("hard")
@@ -53,64 +53,6 @@ router.route('/hard')
     }
   })
 });
-
-
-
-// router.route('/:id')
-// get video details
-  // .get((req, res) => {
-  
-//   const requestedId = req.params.id; 
-  
-//   fs.readFile(videosFilePath, (err, data) => {
-
-//       const requestedVideo = JSON.parse(data).find(video => video.id === requestedId);
-      
-//     if (!requestedVideo) {
-//       return res.status(404).json({ error: 'Video not found' });
-//     }
-//     res.json(requestedVideo);
-//   })
-//   // post comments on a video
-// })
-// router.post('/:id/comments', (req, res) => {
-//   const requestedId = req.params.id; 
-//   const newComment = req.body;
-
-  
-//   fs.readFile(videosFilePath, (err, data) => {
-       
-//     try {
-//       const videos = JSON.parse(data);
-
-//       const requestedVideo = videos.find(video => video.id === requestedId);
-
-//       if (!requestedVideo) {
-//         return res.status(404).json({ error: 'Video not found' });
-//       }
-
-//       if (!requestedVideo.comments) {
-//         requestedVideo.comments = [];
-//       }
-//       requestedVideo.comments.push(newComment);
-
-//       // Write the updated data back to the JSON file
-//       fs.writeFile(videosFilePath, JSON.stringify(videos, null, 2), err => {
-//         if (err) {
-//           console.error('Error writing videos.json:', err);
-//           return res.status(500).json({ error: 'Internal server error' });
-//         }
-
-//         res.json({ message: 'Comment added successfully', video: requestedVideo });
-//       });
-
-//     } catch (parseError) {
-//       console.error('Error parsing JSON:', parseError);
-//       res.status(500).json({ error: 'Error parsing JSON' });
-//     }
-//   });
-// });
-
 
 
 module.exports = {
